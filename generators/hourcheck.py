06ac03947fd58e8d30a12db54de58028a609d725
JasonD <jason.dmail@gmail.com>
2018-10-17 14:13:22 +0200

    Elif.

--- generators/hourcheck.py
+++ generators/hourcheck.py
@@ -9,11 +9,11 @@ def _bgHour(_bgHourDir):
     """Return the path to the correct wallpaper folder."""
     if 0 <= _theHour <= 6:
         _bgHourDir = 'dark'
-    if 6 <= _theHour <= 12:
+    elif 6 <= _theHour <= 12:
         _bgHourDir = 'dawn'
-    if 12 <= _theHour <= 18:
+    elif 12 <= _theHour <= 18:
         _bgHourDir = 'day'
-    if 18 <= _theHour <= 23:
+    elif 18 <= _theHour <= 23:
         _bgHourDir = 'dusk'
     return _bgHourDir
 # end checking Hour

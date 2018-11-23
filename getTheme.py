4721b516d303522870182f0d82c7c1138c407cbf
JasonD <jason.dmail@gmail.com>
2018-11-23 14:13:49 +0100

    getting repo up to date

--- getTheme.py
+++ getTheme.py
@@ -4,17 +4,36 @@
 from generators import colorgen
 import sys
 import os
+import re
 from PIL import Image
 
 if __name__ == "__main__":
 
-    imgLocation = sys.argv[1]
-    imgFolder = os.path.split(imgLocation)[0]
-    img = Image.open(imgLocation).convert('RGB')
+    fehcmd = colorgen.fehfunct()
+    print(fehcmd)
+    os.rename
+    bgLoc = colorgen.currentBg()
+    print(bgLoc)
 
-    hexdict = {}
-    colorgen.populate_dictionary(hexdict, img)
+    imgFolder = os.path.split(bgLoc)[0]
+    print(imgFolder)
+    img = Image.open(bgLoc).convert('RGB')
+
+    print(img)
+    rgbcolors = colorgen.colorList(img)
+    hslcolors = colorgen.hslSort(rgbcolors)
+    print(hslcolors)
+
+    for rgbcolor in rgbcolors:
+        hexdict = colorgen.makeHexDict(rgbcolor)
+    print(hexdict)
+
     hexlist = sorted(hexdict, key=hexdict.get, reverse=True)
     print(hexlist[:16])
 
-    Image.colorgen.colorsort().save('sorted.jpg')
+
+    reghex = re.compile('\#[a-z0-9]*')
+    conkyDefaultColor = colorgen.conkyDefault()
+    conkyNewDefault = re.sub(reghex, hexlist[0], conkyDefaultColor, 1)
+    conkyWindowColor = colorgen.conkyWindow()
+    conkyNewWindow = re.sub(reghex, hexlist[1], conkyWindowColor, 1)

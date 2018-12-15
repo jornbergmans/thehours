317db5cfc18dbc7e69c9a6fb3801a4dc36cd65d7
Jorn Bergmans <jorn.bergmans@gmail.com>
2018-12-15 17:28:43 +0100

    Clean up done on the debug parts that are currently working

--- getTheme.py
+++ getTheme.py
@@ -9,24 +9,15 @@ from PIL import Image
 if __name__ == "__main__":
 
     bgLoc = get.getBg()
-#    imgFolder = os.path.split(bgLoc)[0]
-    img1 = Image.open(bgLoc[1]).convert('RGB')
-    img2 = Image.open(bgLoc[2]).convert('RGB')
+    bgImg1 = Image.open(bgLoc[1]).convert('RGB')
+    bgImg2 = Image.open(bgLoc[2]).convert('RGB')
 
-    rgbcolors = get.colorList(img1)
-    rgbcolors = get.colorList(img2)
+    rgbcolors = get.colorList(bgImg1)
+    rgbcolors = get.colorList(bgImg2)
 
     for rgbcolor in rgbcolors:
         hexdict = get.makeHexDict(rgbcolor)
     hexlist = get.makeHexList(hexdict)
-    # print(hexlist)
 
     set.conkyDefault(hexlist)
     setBg = sp.run(set.fehBg(bgLoc))
-
-
-#    reghex = re.compile('\#[a-z0-9]*')
-#    conkyDefaultColor = generator.conkyDefault()
-#    conkyNewDefault = re.sub(reghex, hexlist[0], conkyDefaultColor, 1)
-#    conkyWindowColor = generator.conkyWindow()
-#    conkyNewWindow = re.sub(reghex, hexlist[1], conkyWindowColor, 1)
